ea6fc84ebfdeb860b4a417aab00aad7d472a36e6
Heiko Krill <heiko.krill@khk.ee>
2021-02-11 15:11:59 +0200

    2.4 Restoraan

---
+++ 2. teema/Restoraan.py
@@ -0,0 +1,8 @@
+class Restoraan():
+    restoraani_nimi = ""
+    soogi_tyyp = ""
+    def restoraani_kirjeldus(self):
+        print("Restoraan " + str(self.restoraani_nimi) + ", söögi tüüp " + str(self.soogi_tyyp))
+
+        def ava_restoraan(self):
+            print("Restoraan on avatud")
--- 2. teema/test.py
+++ 2. teema/test.py
@@ -1,28 +1,6 @@
-import random
-from Sodur import Sodur
+from Restoraan import Restoraan
 # loome 2 eksemplaari Sodur klassi põhjal
-sodur1 = Sodur()
-sodur2 = Sodur()
-# väljastame iga soduri  tervise väärtuse
-print("1. soduri tervis = " + str(sodur1.tervis))
-print("2. soduri tervis = " + str(sodur2.tervis))
-# võistluse algoritm
-while (sodur1.tervis == 0 or sodur2.tervis == 0):
-    soduri_nr = random.randint(1, 2)
-    if(soduri_nr == 1):
-        print("Esimene lööb teist")
-        sodur2.tervis -= 20
-        print("2. soduri tervis = " + str(sodur2.tervis))
-    if (sodur1_nr == 2):
-        print("Teine lööb esimest")
-        sodur1.tervis -= 20
-        print("1. soduri tervis = " + str(sodur1.tervis))
-    if (sodur1.tervis == 0 or sodur2.tervis == 0):
-        break
-# kontrollime sodurite tervist
-if (sodur1.tervis == 0):
-    print("Teine sõdur on võitnud")
-if (sodur2.tervis == 0):
-    print("Esimene  sõdur on võitnud")
-if (sodur1.tervis == 0 and sodur2.tervis == 0):
-    print("Keegi ei võitnud")
+restoraan1.restoraani_nimi = "Heshburger"
+restoraan1.soogi_tyyp = "burgerid"
+restoraan1.restoraani_kirjeldus()
+restoraan1.ava_restoraan()
