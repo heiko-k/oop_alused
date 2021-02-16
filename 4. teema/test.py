a650a15663e6c5c941a1e9517408bca8943f14f5
Heiko Krill <heiko.krill@khk.ee>
2021-02-16 11:02:08 +0200

    Pärilus - Auto klassi laiendamine

---
+++ 4. teema/test.py
@@ -0,0 +1,21 @@
+from Auto import Auto
+
+ainari_uus_auto = Auto("audi", "a6", 2017)
+minu_uus_auto = Auto("toyota", "highLander", 2021)
+
+print(ainari_uus_auto.kirjeldus())
+ainari_uus_auto.odomeeter()
+# 1 - objekti atribuuti otse väärtustamine
+# ainari_uus_auto.odomeetri_nait = 2
+ainari_uus_auto.uuenda_odomeeter(2)
+ainari_uus_auto.odomeeter()
+ainari_uus_auto.suurenda_odomeeter(30)
+ainari_uus_auto.odomeeter()
+
+print(minu_uus_auto.kirjeldus())
+minu_uus_auto.odomeeter()
+
+tesla = Elektriauto("Tesla", "mudel s", 2019)
+print(tesla.kirjeldus())
+tesla.odomeeter()
+tesla.suurenda_odomeeter(50)
