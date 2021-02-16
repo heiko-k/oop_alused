3ad24a3bc643342f30e5de70b6ef35da805f1e17
Heiko Krill <heiko.krill@khk.ee>
2021-02-16 09:26:43 +0200

    kordamine - klassi atribuutite väärtuse määramine võimalused otse meetodite abil

--- 3. teema/Auto.py
+++ 3. teema/Auto.py
@@ -2,9 +2,22 @@ class Auto():
     def __init__(self, t, m, a):
         self.tootja = t
         self.mudel = m
-        self.aasta =
+        self.aasta = a
+        self.odomeetri_nait = 0
 
 
     def kirjeldus(self):
         pikk_nimi = str(self.aasta) + " " + self.tootja + " " + self.mudel
         return pikk_nimi.title()
+
+    def odomeeter(self):
+        print("Antud auto on läbi " +str(self.odomeetri_nait) + " km")
+
+    def uuenda_odomeeter(self, km):
+        if km >= self.odomeetri_nait:
+            self.odomeetri_nait = km
+        else:
+            print("Ei ole võimalik tagasi keerata odomeetri näitu")
+
+    def suurenda_odomeeter(self, km):
+        self.odomeetri_nait += km
--- 3. teema/test.py
+++ 3. teema/test.py
@@ -3,3 +3,9 @@ from Auto import Auto
 minu_uus_auto = auto("audi", "a6", 2017)
 
 print(minu_uus_auto.kirjeldus())
+minu_uus_auto.odomeeter()
+minu_uus_auto.odomeetri_nait = (2)
+minu_uus_auto.odomeeter()
+
+minu_uus_auto.suurenda_odomeeter(30)
+minu_uus_auto.odomeeter()
